1de478c3337cdd50bba6d7968d93565a131aefdf
Vinicius <vinicius.gdechechi@gmail.com>
2024-10-26 17:24:05 -0300

    antes de fazer as migrations

--- src/App/Startup.cs
+++ src/App/Startup.cs
@@ -1,4 +1,5 @@
 using App.Data;
+using Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +31,12 @@ namespace App
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
--- src/Data/Context/MeuDbContext.cs
+++ src/Data/Context/MeuDbContext.cs
@@ -7,9 +7,9 @@ using System.Text;
 
 namespace Data.Context
 {
-    public class MyDbContext : DbContext
+    public class MeuDbContext : DbContext
     {
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options) { }
 
 
         public DbSet<Produto> Produtos { get; set; }
@@ -19,18 +19,17 @@ namespace Data.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             foreach (var property in modelBuilder.Model.GetEntityTypes()
-                .SelectMany(e => e.GetProperties()
-                    .Where(p => p.ClrType == typeof(string))))
-                property.SetColumnType("varchar(100)");
-
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
-
-            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        .SelectMany(e => e.GetProperties()
+            .Where(p => p.ClrType == typeof(string))))
             {
-                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+                property.SetColumnType("varchar(100)");
             }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach(var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
 
 
             base.OnModelCreating(modelBuilder);
